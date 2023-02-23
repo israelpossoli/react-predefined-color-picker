fa5e640e040274d5981c8386be36a2687c833b4b
Israel A. Possoli <israel.possoli@cargolift.com.br>
2023-02-22 21:16:15 -0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-predefined-color-picker'
+import 'react-predefined-color-picker/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
