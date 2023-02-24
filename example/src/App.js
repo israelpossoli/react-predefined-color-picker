8534f64df500e3799572a6b358858e30a4cfa4fd
Israel A. Possoli <israel.possoli@cargolift.com.br>
2023-02-24 13:48:04 -0300

    Prototype 0.1.6

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import ColorPredefinedPicker from 'react-predefined-color-picker'
 
-import { ColorPredefinedPicker } from 'react-predefined-color-picker'
-import 'react-predefined-color-picker/dist/index.css'
 
 const App = () => {
-  return <ColorPredefinedPicker text="Create React Library Example 😄" />
+  return (
+    <div>
+      <ColorPredefinedPicker />
+    </div>
+  )
 }
 
 export default App
